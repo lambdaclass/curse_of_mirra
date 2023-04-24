77f48e33cccb693de53de599ec29d59fc3cfff4d
Javier Chatruc <jrchatruc@gmail.com>
2023-04-24 11:00:37 -0300

    Test

--- mix.exs
+++ mix.exs
@@ -70,7 +70,7 @@ defmodule DarkWorldsServer.MixProject do
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
-      "assets.build": ["npm install --prefix assets", "tailwind default", "esbuild default"],
+      "assets.build": ["tailwind default", "esbuild default"],
       "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
