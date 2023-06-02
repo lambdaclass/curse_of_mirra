051e8cbd7f5e3c2281326739de02fc309adc02c8
An <82987608+AngieDutra@users.noreply.github.com>
2023-06-02 18:18:26 -0300

    spawn and pick up loot

--- client/Assets/Scripts/SpawnLoot.cs
+++ client/Assets/Scripts/SpawnLoot.cs
@@ -1,23 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
+using MoreMountains.Feedbacks;
+using MoreMountains.TopDownEngine;
 using UnityEngine;
 
 public class SpawnLoot : MonoBehaviour
 {
-    GameObject healthPrefab;
+    GameObject lootInstance;
+    // public MMFeedbacks PickedMMFeedbacks;
 
-
-    // Start is called before the first frame update
-    public void Init()
+    public void Initialize()
     {
-        healthPrefab = Instantiate(Resources.Load("LootHealth", typeof(GameObject))) as GameObject;
-        healthPrefab.transform.position = new Vector3(Random.Range(-40f, 50f), 1f, Random.Range(-40f, 50f));
-        healthPrefab.transform.localScale = new Vector3(1.25f, 1.25f, 1.25f);
+        print(lootInstance);
+        if (lootInstance == null)
+        {
+            Init("LootHealth", new Vector3(Random.Range(-30f, 30f), 1f, Random.Range(-30f, 30f)));
+        }
+        else
+        {
+            PickUp();
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    void Init(string prefab, Vector3 position)
     {
-
+        lootInstance = Instantiate(Resources.Load(prefab, typeof(GameObject))) as GameObject;
+        lootInstance.transform.position = position;
+        // lootInstance.GetComponent<PickableItem>().PickedMMFeedbacks.PlayFeedbacks();
+        // PickedMMFeedbacks?.Initialization(lootInstance);
+    }
+    // Update is called once per frame
+    public void PickUp()
+    {
+        print(lootInstance);
+        lootInstance.GetComponent<PickableItem>().PickedMMFeedbacks.PlayFeedbacks();
+        Destroy(lootInstance, 0.1f);
     }
 }
