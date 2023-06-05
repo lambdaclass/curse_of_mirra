276b22429ce443723c2a2847cc67ddfa6d94ebf1
An <82987608+AngieDutra@users.noreply.github.com>
2023-06-05 14:40:49 -0300

    spawn and pick up loot through button

--- client/Assets/Scripts/SpawnLoot.cs
+++ client/Assets/Scripts/SpawnLoot.cs
@@ -1,15 +1,12 @@
-using MoreMountains.Feedbacks;
 using MoreMountains.TopDownEngine;
 using UnityEngine;
 
 public class SpawnLoot : MonoBehaviour
 {
     GameObject lootInstance;
-    // public MMFeedbacks PickedMMFeedbacks;
 
     public void Initialize()
     {
-        print(lootInstance);
         if (lootInstance == null)
         {
             Init("LootHealth", new Vector3(Random.Range(-30f, 30f), 1f, Random.Range(-30f, 30f)));
@@ -19,18 +16,13 @@ public class SpawnLoot : MonoBehaviour
             PickUp();
         }
     }
-    // Start is called before the first frame update
     void Init(string prefab, Vector3 position)
     {
         lootInstance = Instantiate(Resources.Load(prefab, typeof(GameObject))) as GameObject;
         lootInstance.transform.position = position;
-        // lootInstance.GetComponent<PickableItem>().PickedMMFeedbacks.PlayFeedbacks();
-        // PickedMMFeedbacks?.Initialization(lootInstance);
     }
-    // Update is called once per frame
     public void PickUp()
     {
-        print(lootInstance);
         lootInstance.GetComponent<PickableItem>().PickedMMFeedbacks.PlayFeedbacks();
         Destroy(lootInstance, 0.1f);
     }
