0a9d2b78869c7888a3ee25ff2f24d737de48186e
BertovDev <bertobautista18@gmail.com>
2023-06-23 15:57:07 -0300

    Add animation

--- client/Assets/Scripts/UI/KillFeedItem.cs
+++ client/Assets/Scripts/UI/KillFeedItem.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KillFeedItem : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI killerPlayer;
     [SerializeField] TextMeshProUGUI killedPlayer;
+
+    void Start()
+    {
+        GetComponent<Animator>().Play("KillfeedAnim", 0, 0.0f);
+    }
 
     public void SetPlayerNames(string killer, string killed)
     {
