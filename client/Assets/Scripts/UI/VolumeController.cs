d16bbaf0e660024f3063217379aa38a4d4f47477
NContinanza <nicolas.continanza@lambdaclass.com>
2023-06-23 16:24:52 -0300

    Link sliders to each volume controller

--- client/Assets/Scripts/UI/VolumeController.cs
+++ client/Assets/Scripts/UI/VolumeController.cs
@@ -8,17 +8,31 @@ public class VolumeController : MonoBehaviour
 {
     private MMSoundManager soundManager;
     private Slider masterVolumeSlider;
+    private Slider musicVolumeSlider;
+    private Slider sfxVolumeSlider;
 
     // Start is called before the first frame update
     void Awake()
     {
         soundManager = FindObjectOfType<MMSoundManager>();
         masterVolumeSlider = GameObject.Find("MasterVolumeSlider").GetComponent<Slider>();
+        musicVolumeSlider = GameObject.Find("MusicVolumeSlider").GetComponent<Slider>();
+        sfxVolumeSlider = GameObject.Find("SfxVolumeSlider").GetComponent<Slider>();
     }
 
     // Update is called once per frame
-    public void ChangeVolume()
+    public void ChangeMasterVolume()
     {
         soundManager.SetVolumeMaster(masterVolumeSlider.value);
+    }
+
+    public void ChangeMusicVolume()
+    {
+        soundManager.SetVolumeMusic(musicVolumeSlider.value);
+    }
+
+    public void ChangeSfxVolume()
+    {
+        soundManager.SetVolumeSfx(sfxVolumeSlider.value);
     }
 }
