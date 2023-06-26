482f541d48977669cc1605c121f38608932e9476
NContinanza <nicolas.continanza@lambdaclass.com>
2023-06-26 12:00:01 -0300

    Remove unused variable

--- client/Assets/Scripts/UI/VolumeController.cs
+++ client/Assets/Scripts/UI/VolumeController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using MoreMountains.Tools;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,7 +5,6 @@ using UnityEngine.UI;
 public class VolumeController : MonoBehaviour
 {
     private MMSoundManager soundManager;
-    private Slider masterVolumeSlider;
     private Slider musicVolumeSlider;
     private Slider sfxVolumeSlider;
 
