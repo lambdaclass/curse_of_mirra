f62dac739f483e81b45fcea6f99fb5f9e78532a3
An <82987608+AngieDutra@users.noreply.github.com>
2023-06-30 11:34:43 -0300

    dele mono crash and add left clamp

--- client/Assets/Scripts/UI/LeftMMTouchJoystick.cs
+++ client/Assets/Scripts/UI/LeftMMTouchJoystick.cs
@@ -7,28 +7,39 @@ using UnityEngine.EventSystems;
 
 public class LeftMMTouchJoystick : MMTouchRepositionableJoystick
 {
+    float positionX;
+    float positionY;
     protected override void Start()
     {
         base.Start();
         _initialPosition = BackgroundCanvasGroup.transform.position;
         Input.multiTouchEnabled = false;
     }
-    private Vector3 clampedJoystickPositionToScreen(PointerEventData eventData)
+    private Vector3 clampJoystickPositionToScreen(PointerEventData eventData)
     {
         if (eventData.position.y < GetComponent<RectTransform>().position.y + BackgroundCanvasGroup.GetComponent<RectTransform>().sizeDelta.y / 2)
         {
-            _newPosition = new Vector3(eventData.position.x, eventData.position.y + BackgroundCanvasGroup.GetComponent<RectTransform>().sizeDelta.y / 2, 0f);
+            positionY = eventData.position.y + BackgroundCanvasGroup.GetComponent<RectTransform>().sizeDelta.y / 2;
         }
         else
         {
-            _newPosition = eventData.position;
+            positionY = eventData.position.y;
         }
+        if (eventData.position.x < GetComponent<RectTransform>().position.x + BackgroundCanvasGroup.GetComponent<RectTransform>().sizeDelta.x / 2)
+        {
+            positionX = eventData.position.x + BackgroundCanvasGroup.GetComponent<RectTransform>().sizeDelta.x / 2;
+        }
+        else
+        {
+            positionX = eventData.position.x;
+        }
+        _newPosition = new Vector3(positionX, positionY, 0f);
         return _newPosition;
     }
     public override void OnPointerDown(PointerEventData eventData)
     {
         base.OnPointerDown(eventData);
-        clampedJoystickPositionToScreen(eventData);
+        clampJoystickPositionToScreen(eventData);
         BackgroundCanvasGroup.transform.position = _newPosition;
         KnobCanvasGroup.GetComponent<MMTouchJoystick>().SetNeutralPosition(_newPosition);
         KnobCanvasGroup.GetComponent<MMTouchJoystick>().OnPointerDown(eventData);
