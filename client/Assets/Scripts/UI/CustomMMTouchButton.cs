8610e5cfa8a38440177442f5b0d6566f78a0b119
An <82987608+AngieDutra@users.noreply.github.com>
2023-07-03 12:16:29 -0300

    new trigger assets

---
+++ client/Assets/Scripts/UI/CustomMMTouchButton.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using MoreMountains.Tools;
+public class CustomMMTouchButton : MMTouchButton
+{
+    public void SetInitialSprite(Sprite newSprite)
+    {
+        this._initialSprite = newSprite;
+    }
+}
