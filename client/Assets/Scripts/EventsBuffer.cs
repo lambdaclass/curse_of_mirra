5e1c9eb72c584ff28bf59ced7a395275118aeaed
tvillegas98 <tvillegas@fi.uba.ar>
2023-07-05 16:41:07 -0300

    Format event buffer

--- client/Assets/Scripts/EventsBuffer.cs
+++ client/Assets/Scripts/EventsBuffer.cs
@@ -4,38 +4,42 @@ using UnityEngine;
 
 public class EventsBuffer
 {
-  const int bufferLimit = 30;
-  public List<GameEvent> updatesBuffer = new List<GameEvent>();
+    const int bufferLimit = 30;
+    public List<GameEvent> updatesBuffer = new List<GameEvent>();
 
-  public long firstTimestamp = 0;
+    public long firstTimestamp = 0;
 
-  public long deltaInterpolationTime { get; set; }
+    public long deltaInterpolationTime { get; set; }
 
-  public void AddEvent(GameEvent newEvent)
-  {
-    if (updatesBuffer.Count == bufferLimit)
+    public void AddEvent(GameEvent newEvent)
     {
-      updatesBuffer.RemoveAt(0);
+        if (updatesBuffer.Count == bufferLimit)
+        {
+            updatesBuffer.RemoveAt(0);
+        }
+        updatesBuffer.Add(newEvent);
     }
-    updatesBuffer.Add(newEvent);
-  }
 
-  public GameEvent lastEvent()
-  {
-    int lastIndex = updatesBuffer.Count - 1;
-    return updatesBuffer[lastIndex];
-  }
-
-  public GameEvent getNextEventToRender(long pastTime)
-  {
-    GameEvent nextGameEvent = updatesBuffer.Where(ge => ge.ServerTimestamp > pastTime)
-      .OrderBy(ge => ge.ServerTimestamp)
-      .FirstOrDefault();
-
-    if(nextGameEvent == null){
-      return this.lastEvent();
-    }else{
-      return nextGameEvent;
+    public GameEvent lastEvent()
+    {
+        int lastIndex = updatesBuffer.Count - 1;
+        return updatesBuffer[lastIndex];
     }
-  }
+
+    public GameEvent getNextEventToRender(long pastTime)
+    {
+        GameEvent nextGameEvent = updatesBuffer
+            .Where(ge => ge.ServerTimestamp > pastTime)
+            .OrderBy(ge => ge.ServerTimestamp)
+            .FirstOrDefault();
+
+        if (nextGameEvent == null)
+        {
+            return this.lastEvent();
+        }
+        else
+        {
+            return nextGameEvent;
+        }
+    }
 }
