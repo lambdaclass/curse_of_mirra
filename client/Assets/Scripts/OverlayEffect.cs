fc9dbb541c43c4f2c7a5100407b10d9fed0e70ef
BertovDev <bertobautista18@gmail.com>
2023-07-12 17:36:07 -0300

    Add solution without commandBuffers

--- client/Assets/Scripts/OverlayEffect.cs
+++ client/Assets/Scripts/OverlayEffect.cs
@@ -16,34 +16,48 @@ public class OverlayEffect : MonoBehaviour
     public int selectedGroupID = 1;
 
     List<SkinnedMeshRenderer> skinnedMeshFilter = new List<SkinnedMeshRenderer>();
+
     [Header("Objects")]
     public GameObject objectToHighlight;
 
     public void OnEnable()
     {
-        if (camera == null)
-            camera = Camera.main;
+        // if (camera == null)
+        //     camera = Camera.main;
 
-        shader = Shader.Find("Unlit/OverlayShader");
-        material = new Material(shader);
-        commandBuffer = new CommandBuffer();
-        camera = Camera.main;
-        camera.AddCommandBuffer(cameraEvent, commandBuffer);
+        // // shader = Shader.Find("Unlit/OverlayShader");
+        // material = new Material(shader);
+        // commandBuffer = new CommandBuffer();
+        // camera = Camera.main;
+        // camera.AddCommandBuffer(cameraEvent, commandBuffer);
+        // print();
+        objectToHighlight.GetComponentsInChildren(skinnedMeshFilter);
+        foreach (var meshFilter in skinnedMeshFilter)
+        {
+            meshFilter.GetComponent<Renderer>().material.shader = shader;
+        }
     }
 
     public void OnDisable()
     {
-        if (commandBuffer != null && camera != null)
+        // if (commandBuffer != null && camera != null)
+        // {
+        //     camera.RemoveCommandBuffer(cameraEvent, commandBuffer);
+        //     commandBuffer.Release();
+        //     commandBuffer = null;
+        // }
+        objectToHighlight.GetComponentsInChildren(skinnedMeshFilter);
+        foreach (var meshFilter in skinnedMeshFilter)
         {
-            camera.RemoveCommandBuffer(cameraEvent, commandBuffer);
-            commandBuffer.Release();
-            commandBuffer = null;
+            meshFilter.GetComponent<Renderer>().material.shader = Shader.Find(
+                "Universal Render Pipeline/Lit"
+            );
         }
     }
 
-    void LateUpdate()
-    {
-        commandBuffer.Clear();
-        commandBuffer.DrawAllMeshes(objectToHighlight, material, 0);
-    }
+    // void LateUpdate()
+    // {
+    //     commandBuffer.Clear();
+    //     commandBuffer.DrawAllMeshes(objectToHighlight, material, 0);
+    // }
 }
