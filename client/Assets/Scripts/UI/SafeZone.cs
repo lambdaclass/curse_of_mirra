fb998bd5ff173ed7d885d5bd7b8d59d13317d6ed
NContinanza <nicolas.continanza@lambdaclass.com>
2023-07-18 13:12:32 -0300

    Implement hardcoded safe zone shrinking area

---
+++ client/Assets/Scripts/UI/SafeZone.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SafeZone : MonoBehaviour
+{
+    public static SafeZone instance;
+    public float safeZoneRadius;
+    public SpriteMask safeZone;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            return;
+        }
+        instance = this;
+        safeZone = GetComponentInChildren<SpriteMask>();
+        safeZoneRadius = 200f;
+    }
+
+    private void Update()
+    {
+        safeZoneRadius -= Time.deltaTime * 10f;
+        safeZone.transform.localScale = new Vector3(safeZoneRadius, safeZoneRadius, 1);
+    }
+}
