dafa62824ca5c73585036131cc989bec4cffe393
An <82987608+AngieDutra@users.noreply.github.com>
2023-07-25 12:39:44 -0300

    delete debug logs

--- client/Assets/Scripts/UI/SkillDescription.cs
+++ client/Assets/Scripts/UI/SkillDescription.cs
@@ -9,21 +9,15 @@ public class SkillDescription : MonoBehaviour, IPointerDownHandler
     SkillInfo skillData;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("here");
-    }
+    void Start() { }
 
     public void GetCharacter(SkillInfo skill)
     {
-        Debug.Log("get: " + skill.name);
         skillData = skill;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("pointer down: " + this.name);
-        Debug.Log("skill: " + skillData);
         skillName.text = skillData.name;
         skillDescription.text = skillData.description;
     }
