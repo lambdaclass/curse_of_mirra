aa48a2c1b868107b6f6fc75e72a5fc1592abcb1c
BertovDev <bertobautista18@gmail.com>
2023-07-26 15:38:23 -0300

    Add scrolleable container

--- client/Assets/Scripts/CustomLogs.cs
+++ client/Assets/Scripts/CustomLogs.cs
@@ -27,13 +27,19 @@ public class CustomLogs : MonoBehaviour
         Application.logMessageReceivedThreaded -= HandleLog;
     }
 
+    void Update()
+    {
+        CustomDebugLog.CustomDebug("CUSTOMM!!");
+    }
+
     void HandleLog(string logString, string stackTrace, LogType type)
     {
-        if (queue.Count >= maxLines || !debugPrint)
-        {
-            queue.Dequeue();
-            debugLogText.text = " ";
-        }
+        //if we want a max messages length descomment this :)
+        // if (queue.Count >= maxLines || !debugPrint)
+        // {
+        //     queue.Dequeue();
+        //     debugLogText.text = " ";
+        // }
         if (debugPrint)
         {
             queue.Enqueue(logString);
