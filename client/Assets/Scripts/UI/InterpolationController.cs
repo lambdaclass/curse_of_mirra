d4fd299d5795328ddd92bba4f315c89593d236f3
tvillegas98 <tvillegas@fi.uba.ar>
2023-07-28 17:05:06 -0300

    Add the interpolation time in the screen

--- client/Assets/Scripts/UI/InterpolationController.cs
+++ client/Assets/Scripts/UI/InterpolationController.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InterpolationController : MonoBehaviour
 {
     private Slider deltaInterpolationSlider;
+
+    [SerializeField]
+    private TextMeshProUGUI interpolationText;
 
     void Awake()
     {
@@ -15,6 +19,8 @@ public class InterpolationController : MonoBehaviour
     // Update is called once per frame
     public void changeDeltaInterpolationTime()
     {
+        interpolationText.text =
+            "Interpolation Time: " + ((long)deltaInterpolationSlider.value).ToString() + "ms";
         SocketConnectionManager.Instance.eventsBuffer.deltaInterpolationTime = (long)
             deltaInterpolationSlider.value;
     }
