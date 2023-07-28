4ed3859bb181c36be7e1d1f4c290073425c043de
Manuel Camejo <mcamejo@fi.uba.ar>
2023-07-27 21:36:54 -0300

    fix rotation

--- client/Assets/Scripts/PlayerMovement.cs
+++ client/Assets/Scripts/PlayerMovement.cs
@@ -676,6 +676,11 @@ public class PlayerMovement : MonoBehaviour
 
     public RelativePosition getPlayerDirection(Player playerUpdate)
     {
+        if (SocketConnectionManager.Instance.playerId != playerUpdate.Id)
+        {
+            return playerUpdate.Direction;
+        }
+
         var inputFromVirtualJoystick = joystickL is not null;
         var inputFromPhysicalJoystick = Input.GetJoystickNames().Length > 0;
 
