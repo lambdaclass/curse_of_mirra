94c69faf13c493a4d6be775da840b753649d995c
NContinanza <nicolas.continanza@lambdaclass.com>
2023-07-31 17:00:56 -0300

    Improve client and server hashes display

--- client/Assets/Scripts/VersionsHashes.cs
+++ client/Assets/Scripts/VersionsHashes.cs
@@ -6,14 +6,16 @@ public class VersionsHashes : MonoBehaviour
     public bool serverHash;
 
     public void Start()
-	{
-		if(GetComponent<Text>() == null)
-		{
-			Debug.LogWarning ("VersionsHashes requires a GUIText component.");
-			return;
-		}
+    {
+        if (GetComponent<Text>() == null)
+        {
+            Debug.LogWarning("VersionsHashes requires a GUIText component.");
+            return;
+        }
 
-        string hash = serverHash ? "Server# " + SocketConnectionManager.Instance.serverHash : "Client# "+ GitInfo.GetGitHash();
-		GetComponent<Text>().text =  hash;
-	}
+        string hash = serverHash
+            ? "Server #" + SocketConnectionManager.Instance.serverHash
+            : "Client #" + GitInfo.GetGitHash();
+        GetComponent<Text>().text = hash;
+    }
 }
