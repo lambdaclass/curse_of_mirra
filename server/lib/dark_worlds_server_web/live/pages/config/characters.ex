19c8761c64742636c94a87c4728b23918f3a8295
lotuuu <nicolas@lotufo.com.ar>
2023-08-04 13:05:09 -0300

    Add character removal

--- server/lib/dark_worlds_server_web/live/pages/config/characters.ex
+++ server/lib/dark_worlds_server_web/live/pages/config/characters.ex
@@ -16,7 +16,7 @@ defmodule DarkWorldsServerWeb.ConfigLive.Characters do
     "SkillUltimate"
   ]
 
-  def mount(params, session, socket) do
+  def mount(_params, _session, socket) do
     config = Utils.Config.read_config(@config_type)
     {:ok, assign(socket, config: to_form(config), characters: Map.keys(config), new_character_name: "")}
   end
@@ -28,19 +28,36 @@ defmodule DarkWorldsServerWeb.ConfigLive.Characters do
   def handle_event("add_character", %{"name" => new_name}, socket) do
     config = socket.assigns.config.params
 
+    id = map_size(config) |> Integer.to_string()
+    name = if new_name == "", do: id, else: new_name
+
     new_character_config = %{
       Map.new(@default_keys, fn key -> {key, ""} end)
-      | "Name" => new_name,
-        "Id" => map_size(config)
+      | "Name" => name,
+        "Id" => id
     }
 
-    config = Map.put(config, new_name, new_character_config)
+    config = Map.put(config, name, new_character_config)
 
     socket =
       assign(socket,
         config: to_form(config),
-        new_character_name: "",
-        characters: socket.assigns.characters ++ [new_name]
+        characters: socket.assigns.characters ++ [name],
+        new_character_name: ""
+      )
+
+    {:noreply, socket}
+  end
+
+  def handle_event("remove_character", %{"name" => name}, socket) do
+    config =
+      socket.assigns.config.params
+      |> Map.drop([name])
+
+    socket =
+      assign(socket,
+        config: to_form(config),
+        characters: List.delete(socket.assigns.characters, name)
       )
 
     {:noreply, socket}
