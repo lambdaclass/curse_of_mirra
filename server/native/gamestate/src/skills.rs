007c589b24116a477cbe4f52a244ecb7388ea172
Marco <20408709+marco-paulucci@users.noreply.github.com>
2023-08-09 16:34:15 +0200

    temporarily add desc fields to backend

--- server/native/gamestate/src/skills.rs
+++ server/native/gamestate/src/skills.rs
@@ -11,10 +11,15 @@ pub struct Skill {
     pub duration: u64,
     pub skill_range: f64,
     pub par1: u32,
+    pub par1desc: String, // temp. See #764
     pub par2: u32,
+    pub par2desc: String, // temp
     pub par3: u32,
+    pub par3desc: String, // temp
     pub par4: u32,
+    pub par4desc: String, // temp
     pub par5: u32,
+    pub par5desc: String, // temp
     pub angle: u64,
 }
 
@@ -26,10 +31,15 @@ impl Skill {
         let duration = get_skill_field(config, "Duration")?;
         let skill_range = get_skill_field(config, "SkillRange")?;
         let par1 = get_skill_field(config, "Par1")?;
+        let par1desc = get_skill_field(config, "Par1Desc")?;
         let par2 = get_skill_field(config, "Par2")?;
+        let par2desc = get_skill_field(config, "Par2Desc")?;
         let par3 = get_skill_field(config, "Par3")?;
+        let par3desc = get_skill_field(config, "Par3Desc")?;
         let par4 = get_skill_field(config, "Par4")?;
+        let par4desc = get_skill_field(config, "Par4Desc")?;
         let par5 = get_skill_field(config, "Par5")?;
+        let par5desc = get_skill_field(config, "Par5Desc")?;
         let angle = get_skill_field(config, "Angle")?;
         Ok(Self {
             name,
@@ -38,10 +48,15 @@ impl Skill {
             duration,
             skill_range,
             par1,
+            par1desc,
             par2,
+            par2desc,
             par3,
+            par3desc,
             par4,
+            par4desc,
             par5,
+            par5desc,
             angle,
         })
     }
@@ -56,10 +71,15 @@ impl Default for Skill {
             duration: 0,
             skill_range: 100.0,
             par1: 0,
+            par1desc: "".to_string(),
             par2: 0,
+            par2desc: "".to_string(),
             par3: 0,
+            par3desc: "".to_string(),
             par4: 0,
+            par4desc: "".to_string(),
             par5: 0,
+            par5desc: "".to_string(),
             angle: 10,
         }
     }
