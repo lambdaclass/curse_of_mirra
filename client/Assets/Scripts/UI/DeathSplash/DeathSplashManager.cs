aff65429b7e9b66be414cbf26ad87403d00bc76c
An <82987608+AngieDutra@users.noreply.github.com>
2023-09-01 11:27:23 -0300

    ranking

--- client/Assets/Scripts/UI/DeathSplash/DeathSplashManager.cs
+++ client/Assets/Scripts/UI/DeathSplash/DeathSplashManager.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Linq;
 
 public class DeathSplashManager : MonoBehaviour
 {
+    [SerializeField]
+    TextMeshProUGUI amountOfKills;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var ranking = GetRanking();
+        amountOfKills.text = "# " + ranking.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    private int GetRanking()
     {
-        
+        if (PlayerIsWinner())
+        {
+            return 1;
+        }
+        else
+        {
+            return Utils.GetAlivePlayers().Count() + 1;
+        }
+    }
+
+    private bool PlayerIsWinner()
+    {
+        return SocketConnectionManager.Instance.winnerPlayer.Item1 != null
+            && SocketConnectionManager.Instance.winnerPlayer.Item1.Id
+                == LobbyConnection.Instance.playerId;
     }
 }
