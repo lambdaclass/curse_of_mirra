75838402e0f37aa95fe69d408c75bfa7f66b6172
BertovDev <bertobautista18@gmail.com>
2023-09-08 12:47:38 -0300

    Create addresables settings and add logo image to bundle

--- client/Assets/Scripts/UI/TitleScreenController.cs
+++ client/Assets/Scripts/UI/TitleScreenController.cs
@@ -2,19 +2,37 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using System;
+using UnityEngine.Networking;
 
 public class TitleScreenController : MonoBehaviour
 {
     private const string TITLE_SCENE_NAME = "Lobbies";
 
     [SerializeField]
-    CanvasGroup playNowButton;
+    private CanvasGroup playNowButton;
 
     [SerializeField]
-    Image logoImage;
+    public Image logoImage;
+
+    [SerializeField]
+    private AssetLabelReference logoReference;
+
+    [SerializeField]
+    private AssetReference reference;
 
     void Start()
     {
+        bool success = Caching.ClearCache();
+        if (success)
+        {
+            // print("UNABLE TO CLEAR");
+            print(logoImage.sprite.texture.name);
+        }
+        Addressables.InitializeAsync().Completed += Addressables_Completed;
         StartCoroutine(FadeIn(logoImage.GetComponent<CanvasGroup>(), 1f, .1f));
         StartCoroutine(FadeIn(playNowButton, .3f, 1.2f));
     }
@@ -28,10 +46,32 @@ public class TitleScreenController : MonoBehaviour
     {
         yield return new WaitForSeconds(delay);
 
-        for (float i = 0; i <= 1; i += Time.deltaTime / time)
+        if (element != null)
         {
-            element.alpha = i;
-            yield return null;
+            for (float i = 0; i <= 1; i += Time.deltaTime / time)
+            {
+                element.alpha = i;
+                yield return null;
+            }
         }
+    }
+
+    private void Addressables_Completed(AsyncOperationHandle<IResourceLocator> handle)
+    {
+        Addressables.LoadAssetAsync<Sprite>(reference).Completed += (asyncOperationHandle) =>
+        {
+            Sprite sprite = asyncOperationHandle.Result;
+            logoImage.sprite = sprite;
+            print("Asset loaded successfully");
+            print("Height " + sprite.texture.height);
+        };
+
+        // Addressables.LoadAssetAsync<Sprite>(logoReference).Completed += (asyncOperationHandle) =>
+        // {
+        //     Sprite logoSprite = asyncOperationHandle.Result;
+        //     print("Loading Scene...");
+        //     print(logoSprite.texture.height);
+        //     logoImage.sprite = logoSprite;
+        // };
     }
 }
