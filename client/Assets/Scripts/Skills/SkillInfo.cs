a4aaeaa9387f711a1bc2b48e359c952ffeacdc2a
BertovDev <bertobautista18@gmail.com>
2023-09-18 18:29:37 -0300

    Add OnEnable method to setup abitilies from backend

--- client/Assets/Scripts/Skills/SkillInfo.cs
+++ client/Assets/Scripts/Skills/SkillInfo.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MoreMountains.Tools;
+using System.Text.RegularExpressions;
 
 [CreateAssetMenu(fileName = "New Skill Info", menuName = "CoM Skill")]
 public class SkillInfo : ScriptableObject
 {
     public new string name;
+    public new string jsonName;
     public string description;
     public UIType inputType;
 
@@ -37,9 +39,30 @@ public class SkillInfo : ScriptableObject
     [MMEnumCondition("indicatorType", (int)UIIndicatorType.Area)]
     public float skillAreaRadius;
     public bool showCooldown;
+    public float damage;
+    public float cooldown;
+    public ulong duration;
 
     public bool Equals(SkillConfigItem skillConfigItem)
     {
         return this.name.ToLower() == skillConfigItem.Name.ToLower();
+    }
+
+    void OnEnable()
+    {
+        if (LobbyConnection.Instance != null)
+        {
+            foreach (var skill in LobbyConnection.Instance.serverSettings.SkillsConfig.Items)
+            {
+                var regexName = Regex.Replace(this.name, "[^0-9A-Za-z _-]", "");
+                if (regexName.ToLower() == skill.Name.ToLower())
+                {
+                    this.damage = float.Parse(skill.Damage);
+                    this.cooldown = float.Parse(skill.Cooldown);
+                    this.jsonName = skill.Name;
+                    this.duration = ulong.Parse(skill.Duration);
+                }
+            }
+        }
     }
 }
