5e5c9dbbd6bfa63227abd1a997ac42f19a487081
An <82987608+AngieDutra@users.noreply.github.com>
2023-10-20 16:24:55 -0300

    working playmode tests

--- client/Assets/Scripts/UnityTests/PlayModeTests/GameCreationFlow.cs
+++ client/Assets/Scripts/UnityTests/PlayModeTests/GameCreationFlow.cs
@@ -5,32 +5,35 @@ using UnityEngine;
 using UnityEngine.TestTools;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using MoreMountains.Tools;
 
 public class GameCreationFlow : InputTestFixture
 {
-    Mouse mouse;
-
     [SetUp]
     public override void Setup()
     {
         base.Setup();
         SceneManager.LoadScene("Scenes/TitleScreen");
-        mouse = InputSystem.AddDevice<Mouse>();
     }
 
     [UnityTest]
     public IEnumerator TestGameStart()
     {
-        GameObject enterButton = GameObject.Find("Enter");
-        Assert.That(enterButton, Is.Not.Null);
-        yield return null;
+        GameObject enterButton = GameObject.Find("ButtonContainer");
+        string sceneName = SceneManager.GetActiveScene().name;
+        Assert.That(sceneName, Is.EqualTo("TitleScreen"));
+
+        yield return new WaitForSeconds(3f);
+        ClickUI(enterButton);
+
+        yield return new WaitForSeconds(7f);
+        Debug.Log("check scene");
+        sceneName = SceneManager.GetActiveScene().name;
+        Assert.That(sceneName, Is.EqualTo("MainScreen"));
     }
 
-    /* public void ClickUI(GameObject uiElement)
+    public void ClickUI(GameObject uiElement)
     {
-        Camera camera = GameObject.Find("Camera").GetComponent<Camera>();
-        Vector3 screenPos = camera.WorldToScreenPoint(uiElement.transform.position);
-        Set(mouse.position, screenPos);
-        Click(mouse.leftButton);
-    } */
+        uiElement.GetComponent<MMTouchButton>().ButtonReleased.Invoke();
+    }
 }
