5afb0ba6f8e822f24e7f858dcb1f317f78fda6e1
Martina <martina@martina>
2023-10-23 16:47:35 -0300

    add test to enter Lobbies

--- client/Assets/Scripts/UnityTests/PlayModeTests/GameCreationFlow.cs
+++ client/Assets/Scripts/UnityTests/PlayModeTests/GameCreationFlow.cs
@@ -18,20 +18,43 @@ public class GameCreationFlow : InputTestFixture
 
     [UnityTest]
     public IEnumerator TestGameStart()
+    // Goes from Title Screen to Main Screen by clicking the "Enter" button
     {
         GameObject enterButton = GameObject.Find("ButtonContainer");
         string sceneName = SceneManager.GetActiveScene().name;
         Assert.That(sceneName, Is.EqualTo("TitleScreen"));
+        Debug.Log("Enter Title screen - OK");
+
 
         yield return new WaitForSeconds(3f);
+
         ClickUI(enterButton);
+        Debug.Log("Click on Play Now button - OK");
 
         yield return new WaitForSeconds(7f);
-        Debug.Log("check scene");
+
         sceneName = SceneManager.GetActiveScene().name;
         Assert.That(sceneName, Is.EqualTo("MainScreen"));
-    }
+        Debug.Log("Enter Main Screen - OK");
 
+        // Goes from Main Screen to Lobbies screen by clicking on "Play Game"
+
+        GameObject playGameButton = GameObject.Find("PlayGameButton");
+        Assert.That(playGameButton, !Is.Null);
+
+        Debug.Log("Play Game button is present - OK");
+
+        yield return new WaitForSeconds(1f);
+
+        ClickUI(playGameButton);
+        Debug.Log("Click on Play Game button - OK");
+
+        yield return new WaitForSeconds(1f);
+        string lobbiesSceneName = SceneManager.GetActiveScene().name;
+        Assert.That(lobbiesSceneName, Is.EqualTo("Lobbies"));
+        yield return new WaitForSeconds(7f);
+        Debug.Log("Enger Lobbies - OK");
+    }
     public void ClickUI(GameObject uiElement)
     {
         uiElement.GetComponent<MMTouchButton>().ButtonReleased.Invoke();
