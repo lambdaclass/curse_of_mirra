652696d894f70e2dd1743ac6f0975368be497722
agustinesco <agustin.escobar@lambdaclass.com>
2023-10-26 12:41:33 -0300

    Add comments explaining entity detecting matches

--- server/lib/dark_worlds_server/engine/bot_player.ex
+++ server/lib/dark_worlds_server/engine/bot_player.ex
@@ -152,6 +152,7 @@ defmodule DarkWorldsServer.Engine.BotPlayer do
     Runner.play(game_pid, bot_id, %ActionOk{action: :move_with_joystick, value: %{x: x, y: y}, timestamp: nil})
   end
 
+  # Entity detected is an enemy we should try an attack
   defp do_action(bot_id, game_pid, _players, %{
          action: {:try_attack, %{type: :enemy, direction_to_entity: {x, y}} = direction_to_entity}
        }) do
@@ -168,6 +169,7 @@ defmodule DarkWorldsServer.Engine.BotPlayer do
     end
   end
 
+  # Entity detected is a loot crate we should try to pick it
   defp do_action(bot_id, game_pid, _players, %{
          action: {:try_attack, %{direction_to_entity: {x, y}}}
        }) do
