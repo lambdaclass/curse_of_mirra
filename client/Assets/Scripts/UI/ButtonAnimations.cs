9a2f6e9a974268abda9c3603884561e9433e94f2
An <82987608+AngieDutra@users.noreply.github.com>
2023-10-30 11:23:23 -0300

    animations adjustments

--- client/Assets/Scripts/UI/ButtonAnimations.cs
+++ client/Assets/Scripts/UI/ButtonAnimations.cs
@@ -10,7 +10,7 @@ public class ButtonAnimations : MMTouchButton
     public override void OnPointerDown(PointerEventData data)
     {
         base.OnPointerDown(data);
-        transform.DOScale(new Vector3(0.96f, 0.96f, 0.96f), 0.5f);
+        transform.DOScale(new Vector3(0.965f, 0.965f, 0.965f), 0.5f);
     }
 
     public override void OnPointerUp(PointerEventData data)
