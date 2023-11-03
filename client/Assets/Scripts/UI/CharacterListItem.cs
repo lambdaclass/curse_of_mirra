0d3d3a3c47bbf84e4ed1ab3073792332b0e9f554
BertovDev <bertobautista18@gmail.com>
2023-11-03 15:59:57 -0300

    Fix scroll in characterList

--- client/Assets/Scripts/UI/CharacterListItem.cs
+++ client/Assets/Scripts/UI/CharacterListItem.cs
@@ -2,14 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MoreMountains.Tools;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class CharacterListItem : MonoBehaviour
+public class CharacterListItem
+    : MonoBehaviour,
+        IPointerExitHandler,
+        IPointerEnterHandler,
+        IPointerUpHandler,
+        IPointerDownHandler
 {
     public int listPosition;
+    public bool isInsideCard = false;
+    public bool isRealesed = false;
+    Vector2 touchStartPos;
 
-    public void SetCharacterInfoStart()
+    public void SetCharacterInfoStart(PointerEventData eventData)
     {
-        this.GetComponent<MMLoadScene>().LoadScene();
-        CharacterInfoManager.selectedCharacterPosition = listPosition;
+        if (isInsideCard && eventData.position == touchStartPos)
+        {
+            this.GetComponent<MMLoadScene>().LoadScene();
+            CharacterInfoManager.selectedCharacterPosition = listPosition;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isInsideCard = false;
+        isRealesed = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isInsideCard = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        SetCharacterInfoStart(eventData);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        touchStartPos = eventData.position;
     }
 }
