3b9f92482ac8ed84fe6299077a51998ea9fb599d
An <82987608+AngieDutra@users.noreply.github.com>
2023-11-07 14:51:50 -0300

    force click

--- client/Assets/Scripts/UnityTests/PlayModeTests/MatchStartFlow.cs
+++ client/Assets/Scripts/UnityTests/PlayModeTests/MatchStartFlow.cs
@@ -8,11 +8,14 @@ using UnityEngine.SceneManagement;
 
 public class MatchStartFlow : InputTestFixture
 {
+    Mouse mouse;
+
     [SetUp]
     public override void Setup()
     {
         base.Setup();
         SceneManager.LoadScene("Scenes/Lobbies");
+        mouse = InputSystem.AddDevice<Mouse>();
     }
 
     // Creates a lobby in the Lobby scene
@@ -30,7 +33,9 @@ public class MatchStartFlow : InputTestFixture
         Assert.That(SceneManager.GetActiveScene().name, Is.EqualTo("CharacterSelection"));
         yield return new WaitForFixedUpdate();
 
-        UICharacterItem H4ckUICharacterItem = GameObject.Find("H4ck").GetComponent<UICharacterItem>();
+        UICharacterItem H4ckUICharacterItem = GameObject
+            .Find("H4ck")
+            .GetComponent<UICharacterItem>();
         Assert.That(H4ckUICharacterItem.selected, Is.EqualTo(false));
         yield return TestingUtils.ForceClick("H4ck");
         yield return new WaitForSeconds(2f);
@@ -41,4 +46,11 @@ public class MatchStartFlow : InputTestFixture
         yield return new WaitForSeconds(2f);
         Assert.That(SceneManager.GetActiveScene().name, Is.EqualTo("Battle"));
     }
+    /* public void ClickUI(GameObject uiElement)
+    {
+        Camera camera = GameObject.Find("Camera").GetComponent<Camera>();
+        Vector3 screenPos = camera.WorldToScreenPoint(uiElement.transform.position);
+        Set(mouse.position, screenPos);
+        Click(mouse.leftButton);
+    } */
 }
