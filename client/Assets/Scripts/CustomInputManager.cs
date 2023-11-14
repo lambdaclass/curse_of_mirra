40479852195ea1432a78dbacefd6b894a7f419d4
Manuel Camejo <mcamejo@fi.uba.ar>
2023-11-14 16:01:37 -0300

    auto attack

--- client/Assets/Scripts/CustomInputManager.cs
+++ client/Assets/Scripts/CustomInputManager.cs
@@ -324,10 +324,10 @@ public class CustomInputManager : InputManager
         activeJoystick = null;
         EnableButtons();
 
-        if (direction.x == 0 && direction.y == 0 && skill.ExecutesOnQuickTap())
-        {
-            direction = GetPlayerOrientation();
-        }
+        // if (direction.x == 0 && direction.y == 0 && skill.ExecutesOnQuickTap())
+        // {
+        //     direction = GetPlayerOrientation();
+        // }
 
         if (!activeJoystickStatus)
         {
