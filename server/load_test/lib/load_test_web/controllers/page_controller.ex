fd16a0bd80485b324e9cc6a70507a5154bdd14b9
Martin Fraga <martin.fraga@lambdaclass.com>
2023-11-23 17:11:36 -0300

    show login and logout button, and live dashboard button

--- server/load_test/lib/load_test_web/controllers/page_controller.ex
+++ server/load_test/lib/load_test_web/controllers/page_controller.ex
@@ -2,8 +2,9 @@ defmodule LoadTestWeb.PageController do
   use LoadTestWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    base_url = LoadTestWeb.Endpoint.url()
+    oauth_google_url = ElixirAuthGoogle.generate_oauth_url(base_url)
+    render(conn, "home.html",[oauth_google_url: oauth_google_url])
   end
+
 end
