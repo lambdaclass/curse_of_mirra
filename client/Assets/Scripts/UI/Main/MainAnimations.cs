1dc3d1e8da701f7bb4fd38afcfbca721dd8fdd10
An <82987608+AngieDutra@users.noreply.github.com>
2023-11-28 19:05:35 -0300

    draft

---
+++ client/Assets/Scripts/UI/Main/MainAnimations.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class MainAnimations : MonoBehaviour
+{
+    [SerializeField]
+    Image thunders;
+
+    void Start()
+    {
+        //thunders.DOFade(1, 1);
+    }
+}
