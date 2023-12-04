d9fd10495bc504d8b2403717a7fb675aab179d95
Gonzalo Torres <gonnza@gmail.com>
2023-12-04 19:21:26 -0300

    Improved araban item feedbacks

--- client/Assets/Scripts/LootableEffects.cs
+++ client/Assets/Scripts/LootableEffects.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class LootableEffects : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        // StartCoroutine(LootableIdleAnimation());
-    }
-
     private void Update() {
         transform.position = new Vector3(transform.position.x, (Mathf.Sin(Time.time * 5) * 0.1f) + 1, transform.position.z);
         transform.Rotate(0, 0, 50 * Time.deltaTime);
-        // transform.Rotate(Vector3.up, Time.deltaTime, Space.World);
     }
 }
