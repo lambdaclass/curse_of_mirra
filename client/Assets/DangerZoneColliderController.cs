626bbd13947f92635f3673caf618babdb5864764
BertovDev <bertobautista18@gmail.com>
2023-12-04 18:42:54 -0300

    Implement new dangerzone logic

---
+++ client/Assets/DangerZoneColliderController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerZoneColliderController : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(SetupCollitions());
+    }
+
+    IEnumerator SetupCollitions()
+    {
+        yield return new WaitUntil(() => SocketConnectionManager.Instance.players.Count != 0);
+        Utils
+            .GetAllCharacters()
+            .ForEach(character =>
+            {
+                Physics.IgnoreCollision(
+                    this.GetComponent<MeshCollider>(),
+                    character.GetComponent<CharacterController>()
+                );
+            });
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "ZoneParticle")
+        {
+            other.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+}
--- client/Assets/Scripts/UI/SafeZone.cs
+++ client/Assets/Scripts/UI/SafeZone.cs
@@ -1,3 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.VFX;
 
@@ -12,11 +16,42 @@ public class SafeZone : MonoBehaviour
     [SerializeField]
     GameObject particle;
 
-    GameObject part = new GameObject();
+    [SerializeField]
+    GameObject mesh;
+    float x = 33.51848f;
 
-    float intialRadius = 0.0f;
+    float auxRadius;
+    Vector3 auxCenter = Vector3.zero;
 
-    bool first = false;
+    void Start()
+    {
+        mesh = Instantiate(mesh, Vector3.zero, Quaternion.identity);
+        StartCoroutine(SetupCollitions());
+    }
+
+    IEnumerator SetupCollitions()
+    {
+        yield return new WaitUntil(() => SocketConnectionManager.Instance.players.Count != 0);
+        List<SphereCollider> colliders = GameObject.FindObjectsOfType<SphereCollider>().ToList();
+        Utils
+            .GetAllCharacters()
+            .ForEach(character =>
+            {
+                colliders.ForEach(
+                    collider =>
+                        Physics.IgnoreCollision(
+                            collider,
+                            character.GetComponent<CharacterController>()
+                        )
+                );
+            });
+    }
+
+    public void ActivateParticleEffects(float radius, Vector3 center)
+    {
+        mesh.transform.position = new Vector3(center.x, 1f, center.z);
+        mesh.transform.localScale = new Vector3(radius, 10, radius);
+    }
 
     private void Update()
     {
@@ -28,23 +63,16 @@ public class SafeZone : MonoBehaviour
             SocketConnectionManager.Instance.shrinkingCenter
         );
 
+        auxRadius = radius;
+        auxCenter = center;
+
         float radiusCorrected = radius + radius * .007f;
 
-        if (radius == 200 && !first)
-        {
-            intialRadius = radius;
-            part = Instantiate(particle);
-            part.GetComponent<VisualEffect>().SetFloat("CircleRadius", radiusCorrected);
-            part.transform.position = new Vector3(center.x, 1f, center.z);
-            first = true;
-        }
+        ActivateParticleEffects(radius / 3f, new Vector3(center.x, 1f, center.z));
+    }
 
-        if (intialRadius - radius == 9)
-        {
-            intialRadius = radius;
-            part = Instantiate(particle);
-            part.GetComponent<VisualEffect>().SetFloat("CircleRadius", radiusCorrected);
-            part.transform.position = new Vector3(center.x, 1f, center.z);
-        }
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(auxCenter, auxRadius);
     }
 }
