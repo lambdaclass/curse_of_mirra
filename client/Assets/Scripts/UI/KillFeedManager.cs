7339a5a16fb46ef36ac5b61961be85845c1f7d07
An <82987608+AngieDutra@users.noreply.github.com>
2023-12-07 17:04:46 -0300

    Victory and death splash (#1234)
    * death splashes assets and ui
    
    * draft
    
    * death splash functionality
    
    * death splash to end game scene
    
    * more new styles to end game screen
    
    * death splash in battle
    
    * death splash animations
    
    * killer icon and winner end game screen fix
    
    * fix killfeed
    
    * scriptable objects in death splash
    
    * killfeed fixes
    
    * killzone icons fix
    
    * improve conditional for endgame screen
    
    * death splash prefab

--- client/Assets/Scripts/UI/KillFeedManager.cs
+++ client/Assets/Scripts/UI/KillFeedManager.cs
@@ -7,12 +7,20 @@ public class KillFeedManager : MonoBehaviour
     [SerializeField]
     KillFeedItem killFeedItem;
 
+    [SerializeField]
+    public List<CoMCharacter> charactersScriptableObjects;
+
+    [SerializeField]
+    public Sprite zoneIcon;
+
     public static KillFeedManager instance;
     private Queue<KillEvent> feedEvents = new Queue<KillEvent>();
 
     public ulong saveKillerId;
+    public ulong myKillerId;
 
     public ulong playerToTrack;
+    private const string ZONE_ID = "9999";
 
     public void Awake()
     {
@@ -36,6 +44,23 @@ public class KillFeedManager : MonoBehaviour
         return killerId;
     }
 
+    Sprite GetUIIcon(ulong killerId)
+    {
+        if (killerId.ToString() == ZONE_ID)
+        {
+            return zoneIcon;
+        }
+        else
+        {
+            CoMCharacter characterIcon =
+                KillFeedManager.instance.charactersScriptableObjects.Single(
+                    characterSO =>
+                        characterSO.name.Contains(Utils.GetCharacter(killerId).CharacterModel.name)
+                );
+            return characterIcon.UIIcon;
+        }
+    }
+
     public void Update()
     {
         KillEvent killEvent;
@@ -46,13 +71,19 @@ public class KillFeedManager : MonoBehaviour
                 saveKillerId = killEvent.KilledBy;
                 playerToTrack = saveKillerId;
             }
+            if (killEvent.Killed == SocketConnectionManager.Instance.playerId)
+            {
+                myKillerId = killEvent.KilledBy;
+            }
             // TODO: fix this when the player names are fixed in the server.
             // string deathPlayerName = LobbyConnection.Instance.playersIdName[killEvent.Killed];
             // string killerPlayerName = LobbyConnection.Instance.playersIdName[killEvent.KilledBy];
             string deathPlayerName = killEvent.Killed.ToString();
             string killerPlayerName = killEvent.KilledBy.ToString();
+            Sprite killerIcon = GetUIIcon(killEvent.KilledBy);
+            Sprite killedIcon = GetUIIcon(killEvent.Killed);
 
-            killFeedItem.SetPlayerNames(killerPlayerName, deathPlayerName);
+            killFeedItem.SetPlayerData(killerPlayerName, killerIcon, deathPlayerName, killedIcon);
             GameObject item = Instantiate(killFeedItem.gameObject, transform);
             Destroy(item, 3.0f);
         }
