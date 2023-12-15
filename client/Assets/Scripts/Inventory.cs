32aa2432182baadb431c8b3208146db168e1b00e
An <82987608+AngieDutra@users.noreply.github.com>
2023-12-15 18:01:38 -0300

    animation duration optimization

--- client/Assets/Scripts/Inventory.cs
+++ client/Assets/Scripts/Inventory.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 using System.Collections;
-using MoreMountains.TopDownEngine;
 
 public class Inventory : MonoBehaviour
 {
     private const string MYRRAS_BLESSING = "loot_health";
     private const string ITEM_FEEDBACK = "UseItemEffect";
+
+    // This is the base duration
+    // The animations use this as a base for all the animation durations
+    // If the goal is to change an entire animation duration, this is the value to change
+    const float BASE_DURATION = 0.5f;
     private Player player;
     private GameLoot activeItem;
 
@@ -21,7 +25,8 @@ public class Inventory : MonoBehaviour
 
     [SerializeField]
     Image inventoryImage;
-    Vector3 imageInitialScale;
+    Vector3 imageInitialScale,
+        scaleVariationForPulseAnimation;
     Coroutine pickItemAnimation,
         useItemAnimation;
     Sequence pickSequenceAnimation,
@@ -30,6 +35,7 @@ public class Inventory : MonoBehaviour
     private void Start()
     {
         imageInitialScale = inventoryImage.gameObject.transform.localScale;
+        scaleVariationForPulseAnimation = new Vector3(.15f, .15f, .15f);
     }
 
     IEnumerator AnimatePickItem(Coroutine useItemAnimation)
@@ -38,11 +44,17 @@ public class Inventory : MonoBehaviour
         inventoryContainer.SetActive(true);
         pickSequenceAnimation = DOTween.Sequence();
         pickSequenceAnimation
-            .Append(inventoryContainer.GetComponent<CanvasGroup>().DOFade(1, 0.2f))
-            .Insert(0.1f, inventoryImage.GetComponent<CanvasGroup>().DOFade(1, 0.3f))
+            .Append(inventoryContainer.GetComponent<CanvasGroup>().DOFade(1, BASE_DURATION - 0.3f))
+            .Insert(
+                0.1f,
+                inventoryImage.GetComponent<CanvasGroup>().DOFade(1, BASE_DURATION - 0.2f)
+            )
             .Append(
                 inventoryImage.transform
-                    .DOScale(imageInitialScale + new Vector3(.15f, .15f, .15f), 1)
+                    .DOScale(
+                        imageInitialScale + scaleVariationForPulseAnimation,
+                        BASE_DURATION + 0.5f
+                    )
                     .SetLoops(-1, LoopType.Yoyo)
                     .SetEase(Ease.InOutQuad)
             );
@@ -58,14 +70,17 @@ public class Inventory : MonoBehaviour
         useSequenceAnimation = DOTween.Sequence();
         sparkleEffect.SetActive(true);
         useSequenceAnimation
-            .Append(inventoryImage.transform.DOScale(imageInitialScale * 3, 0.7f))
-            .Insert(0, inventoryImage.GetComponent<CanvasGroup>().DOFade(0, 0.5f))
-            .Insert(0, inventoryContainer.GetComponent<CanvasGroup>().DOFade(0, 0.3f))
+            .Append(inventoryImage.transform.DOScale(imageInitialScale * 3, BASE_DURATION + 0.2f))
+            .Insert(0, inventoryImage.GetComponent<CanvasGroup>().DOFade(0, BASE_DURATION))
+            .Insert(
+                0,
+                inventoryContainer.GetComponent<CanvasGroup>().DOFade(0, BASE_DURATION - 0.2f)
+            )
             .Append(inventoryImage.transform.DOScale(imageInitialScale, 0));
         yield return new WaitForSeconds(0.1f);
         PlayerFeedback(player, true);
 
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(BASE_DURATION);
         sparkleEffect.SetActive(false);
         inventoryContainer.SetActive(false);
         inventoryImage.sprite = null;
