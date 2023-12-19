d7e52d761b4e1132a38d1b7c54051bcfac1ff4ef
An <82987608+AngieDutra@users.noreply.github.com>
2023-12-19 15:47:02 -0300

    Character list updates (#1272)
    * character list updates
    
    * refactor link to character info
    
    * link to character info from main
    
    * model click section adjust
    
    * character info text fix

---
+++ client/Assets/Scripts/CharactersManager.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactersManager : MonoBehaviour
+{
+    public static CharactersManager Instance;
+
+    [SerializeField]
+    public List<CoMCharacter> characterSriptableObjects;
+    public List<CoMCharacter> availableCharacters = new List<CoMCharacter>();
+
+    // The available names should come from backend
+    private List<string> availableCharacterNames = new List<string>() { "Muflus" };
+    public string selectedCharacterName = "Muflus";
+    public string goToCharacterName;
+
+    void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+        availableCharacters = Utils.GetOnlyAvailableCharacterInfo(availableCharacterNames);
+    }
+}
