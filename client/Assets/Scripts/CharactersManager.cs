0abac7f1bcaf980fbb8a5e5b1098fab471032b1c
An <82987608+AngieDutra@users.noreply.github.com>
2024-02-09 14:26:04 -0300

    block valtimer and uma

--- client/Assets/Scripts/CharactersManager.cs
+++ client/Assets/Scripts/CharactersManager.cs
@@ -25,13 +25,7 @@ public class CharactersManager : MonoBehaviour
     }
 
     // The available names should come from backend
-    private List<string> availableCharacterNames = new List<string>()
-    {
-        "Muflus",
-        "H4ck",
-        "Uma",
-        "Valtimer"
-    };
+    private List<string> availableCharacterNames = new List<string>() { "Muflus", "H4ck" };
 
     public string GoToCharacter;
 
