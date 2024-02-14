a3700fa3384eb2930a1526c6a72a54706fd1e832
BertovDev <bertobautista18@gmail.com>
2024-02-14 15:24:52 -0300

    Implement new stamina layout

---
+++ client/Assets/Scripts/StaminaManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaManager : MonoBehaviour
+{
+    [SerializeField] private List<Image> staminaFillImage;
+
+    private ulong maxStamina;    
+    private Player player;
+    private ulong currentStamina = 2;
+
+    void Start(){
+          player = Utils.GetGamePlayer(GameServerConnectionManager.Instance.playerId).Player;
+          maxStamina = player.MaxStamina;
+    }
+   
+    void Update(){
+         player = Utils.GetGamePlayer(GameServerConnectionManager.Instance.playerId).Player;
+         ulong availableStamina = player.AvailableStamina;
+
+        staminaFillImage.ForEach(el => {
+            int index = staminaFillImage.IndexOf(el);
+            if(availableStamina == 0){
+                el.gameObject.SetActive(false);
+            } else {
+              el.gameObject.SetActive(index < (int)availableStamina);
+            }
+        });
+    }
+}
