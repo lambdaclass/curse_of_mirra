b2ec92fad4399fbf207a12fbf8ed2aa6ce60e750
Agustin Escobar <106101218+agustinesco@users.noreply.github.com>
2024-02-24 01:48:06 -0300

    Receive stamina costs from backend (#1498)
    * Assign stamina cost to skillinfo
    
    * Check that player can cast skill by stamina
    
    * Receive and assign stamina form the back

--- client/Assets/Scripts/CustomLevelManager.cs
+++ client/Assets/Scripts/CustomLevelManager.cs
@@ -230,15 +230,16 @@ public class CustomLevelManager : LevelManager
 
     private List<SkillInfo> InitSkills(CoMCharacter characterInfo, string id)
     {
+        ConfigCharacter configCharacter = GameServerConnectionManager.Instance.config.Characters.ToList().Find(character => character.Name == characterInfo.name.ToLower());
         List<SkillInfo> skills = new List<SkillInfo>();
-        characterInfo
-            .skillsInfo
-            .ForEach(skill =>
-            {
-                SkillInfo skillClone = Instantiate(skill);
-                skillClone.InitWithBackend(id);
-                skills.Add(skillClone);
-            });
+        List<ConfigSkill> configSkills = configCharacter.Skills.Values.ToList();
+        for (int index = 0; index < characterInfo.skillsInfo.Count; index++)
+        {
+            SkillInfo skillClone = Instantiate(characterInfo.skillsInfo[index]);
+            skillClone.InitWithBackend(configSkills[index], id);
+            skills.Add(skillClone);
+
+        }
 
         return skills;
     }
@@ -271,6 +272,8 @@ public class CustomLevelManager : LevelManager
                 .Instance
                 .AvailableCharacters
                 .Find(el => el.name.ToLower() == player.CharacterModel.name.ToLower());
+
+
 
             List<SkillInfo> skillInfoClone = InitSkills(characterInfo, player.PlayerID);
             // SetSkillAngles(skillInfoClone);
