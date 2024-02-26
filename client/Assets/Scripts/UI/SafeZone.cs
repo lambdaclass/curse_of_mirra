c9585907e86d0a80ff3238c0b66e25bc55c42bcc
Dmytro Tkachuk <d.tkachuk@pinglestudio.com>
2024-02-26 18:17:05 +0200

    Squased commits

--- client/Assets/Scripts/UI/SafeZone.cs
+++ client/Assets/Scripts/UI/SafeZone.cs
@@ -8,12 +8,13 @@ using UnityEngine.VFX;
 public class SafeZone : MonoBehaviour
 {
     [SerializeField]
-    GameObject smoke;
-
-    [SerializeField]
     float smokeSize = 40f;
 
+    [SerializeField]
+    VisualEffect damageFog = null;
+
     float previusRadius = Mathf.Infinity;
+    private string DAMAGE_ZONE_PROPERTY = "DamageZonePercent";
 
     const float mapRadius = 50;
     Vector3 center = new Vector3(0, 1, 0);
@@ -23,37 +24,11 @@ public class SafeZone : MonoBehaviour
         // We have a difference of x100 between the backend and frontend values
         float radius = GameServerConnectionManager.Instance.playableRadius / 100;
         bool zoneEnabled = GameServerConnectionManager.Instance.zoneEnabled;
+        damageFog.enabled = zoneEnabled;
 
         if (radius != 0 && zoneEnabled)
         {
-            // Vector3 center = Utils.transformBackendOldPositionToFrontendPosition(
-            //     GameServerConnectionManager.Instance.shrinkingCenter
-            // );
-            // float radius = Utils.transformBackendRadiusToFrontendRadius(
-            //     GameServerConnectionManager.Instance.playableRadius
-            // );
-            if ((radius < previusRadius) && (radius <= previusRadius - (smokeSize * 1.5)))
-            {
-                GenerateSmokeRing(radius, center);
-                previusRadius = radius;
-            }
-        }
-    }
-
-    void GenerateSmokeRing(float radius, Vector3 center)
-    {
-        float perimeter = 2.0f * Mathf.PI * (radius);
-        int totalSmokes = Mathf.RoundToInt(perimeter / smokeSize);
-
-        for (int i = 0; i < totalSmokes; i++)
-        {
-            float angle = i * (360 / totalSmokes + 1);
-
-            Vector3 pos = CalculatePositionInCircle(center, radius, angle);
-            if (Mathf.Abs(pos.x) < mapRadius && Mathf.Abs(pos.z) < mapRadius)
-            {
-                Instantiate(smoke, pos, smoke.transform.rotation);
-            }
+            damageFog.SetFloat(DAMAGE_ZONE_PROPERTY, 1 - (radius / mapRadius));
         }
     }
 
