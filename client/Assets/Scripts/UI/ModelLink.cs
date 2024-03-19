efc3619aa78e1509a95d2178d6475ff2dbc1c1d2
An <82987608+AngieDutra@users.noreply.github.com>
2024-03-19 14:38:41 -0300

    fix reset rotation for model and link

---
+++ client/Assets/Scripts/UI/ModelLink.cs
@@ -0,0 +1,62 @@
+using System;
+using MoreMountains.Tools;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ModelLink : MMTouchButton, IDragHandler
+{
+    [SerializeField]
+    RotateUIModel uiModelRotator;
+    bool isInsideContainer;
+    Vector3 touchStartPos;
+    private const float MIN_DIFFERENCE = 10.0f;
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        base.OnPointerDown(eventData);
+        uiModelRotator.OnPointerDown(eventData);
+        touchStartPos = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        uiModelRotator.OnDrag(eventData);
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        if (PointerGoToAction(eventData))
+        {
+            base.OnPointerUp(eventData);
+        }
+        uiModelRotator.OnPointerUp(eventData);
+    }
+
+    bool PointerGoToAction(PointerEventData eventData)
+    {
+        bool executeAction;
+        var touchXDifference = Math.Abs(eventData.position.x - touchStartPos.x);
+        var touchYDifference = Math.Abs(eventData.position.y - touchStartPos.y);
+        if (isInsideContainer)
+        {
+            executeAction = touchXDifference < MIN_DIFFERENCE && touchYDifference < MIN_DIFFERENCE;
+        }
+        else
+        {
+            executeAction = isInsideContainer;
+        }
+        return executeAction;
+    }
+
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        base.OnPointerExit(eventData);
+        isInsideContainer = false;
+    }
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        base.OnPointerEnter(eventData);
+        isInsideContainer = true;
+    }
+}
