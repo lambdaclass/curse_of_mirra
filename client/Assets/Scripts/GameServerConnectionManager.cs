1f6b873fbd09193d4baad6f348452c82105c2e92
AngieDutra <angiedutraa@gmail.com>
2024-05-03 13:02:21 -0300

    Revert "set tested values for truest results"
    This reverts commit 033590e1b0a92bdb2fa1bb128e6681900b95763b.

--- client/Assets/Scripts/GameServerConnectionManager.cs
+++ client/Assets/Scripts/GameServerConnectionManager.cs
@@ -244,7 +244,7 @@ public class GameServerConnectionManager : MonoBehaviour
 
     private string makeWebsocketUrl(string path)
     {
-        int port = 5000;
+        int port = 4000;
 
         if (serverIp.Contains("localhost"))
         {
