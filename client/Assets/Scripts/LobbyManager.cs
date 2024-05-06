17f6b63a063cb0b6001a44a7075857af6a151117
BertovDev <bertobautista18@gmail.com>
2024-05-06 17:39:03 -0300

    Add dynamic types

--- client/Assets/Scripts/LobbyManager.cs
+++ client/Assets/Scripts/LobbyManager.cs
@@ -1,7 +1,12 @@
+// using System.Collections;
 using System.Collections;
+using System.IO;
+using System.Linq;
 using MoreMountains.TopDownEngine;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LobbyManager : LevelSelector
 {
@@ -10,11 +15,37 @@ public class LobbyManager : LevelSelector
     private const string MAIN_SCENE_NAME = "MainScreen";
     private const string LOBBIES_BACKGROUND_MUSIC = "LobbiesBackgroundMusic";
 
+    [SerializeField] TextMeshProUGUI tipText;
+    [SerializeField] Image characterImage;
+
     public static string LevelSelected;
 
     void Start()
     {
         StartCoroutine(WaitForLobbyJoin());
+        if(SceneManager.GetActiveScene().ToString() != BATTLE_SCENE_NAME){
+            ShowCharacterTips();
+        }
+    }
+
+
+    private void ShowCharacterTips(){
+        var characterSelected = ServerConnection.Instance.selectedCharacterName;
+        print(characterSelected);
+        string filePath = Path.Combine(Application.dataPath, "Resources/tips.json");
+        string jsonData = File.ReadAllText(filePath);
+        CharactersData charactersData = JsonUtility.FromJson<CharactersData>(jsonData);
+ 
+        foreach (CharacterTips character in charactersData.characters)
+        {
+            if(character.name.ToUpper() == characterSelected.ToUpper()){
+                int random = Random.Range(0,character.tips.Count());
+                string abilityTip = character.tips[random];
+
+                tipText.text = abilityTip;
+                characterImage.sprite = CharactersManager.Instance.AllCharacters.Find(character => character.name.ToUpper() == characterSelected.ToUpper()).UIIcon;
+            }
+        }
     }
 
     public void BackToLobbyFromGame()
@@ -57,4 +88,33 @@ public class LobbyManager : LevelSelector
         );
         SceneManager.LoadScene(BATTLE_SCENE_NAME);
     }
+
+
+    [System.Serializable]
+    public class CharactersData
+    {
+        public CharacterTips[] characters;
+    }
+
+    [System.Serializable]
+    public class CharacterTips
+    {
+        public string name;
+        public string[] tips;
+    }
+
+    public static class JsonHelper
+{
+    public static T[] FromJson<T>(string json)
+    {
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+        return wrapper.items;
+    }
+
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+        public T[] items;
+    }
+}
 }
