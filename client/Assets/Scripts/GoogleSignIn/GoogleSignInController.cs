e8c2f505e31693ce91a3bf776fe60fc27897cc60
BertovDev <bertobautista18@gmail.com>
2024-05-14 15:48:52 -0300

    Redo clientid

--- client/Assets/Scripts/GoogleSignIn/GoogleSignInController.cs
+++ client/Assets/Scripts/GoogleSignIn/GoogleSignInController.cs
@@ -21,7 +21,7 @@ public class GoogleSignInController : MonoBehaviour
 
     [SerializeField]
     private GameObject signInButton;
-    public string webClientIdGoogle = "273396836446-bpoko025ahovlv9pfs5rtsg7dfm1mrno.apps.googleusercontent.com";
+    public string webClientIdGoogle = "194682062935-ukqi0s2vp1d2nmoembp0dapes21ei859.apps.googleusercontent.com";
 
     private GoogleSignInConfiguration configuration;
 
