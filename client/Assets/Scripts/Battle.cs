3d59fdc08580b7d0fd40aa73ffa9f36426efcec1
Theo <theokatz@gmail.com>
2024-06-10 12:59:13 -0300

    destroy pools when stopped receiving them from backend

--- client/Assets/Scripts/Battle.cs
+++ client/Assets/Scripts/Battle.cs
@@ -743,14 +743,14 @@ public class Battle : MonoBehaviour
     {
         int poolId;
         VisualEffect poolVFX;
-        Color red = new Color(.85f, .075f, .1f, 0f);
-        Color blue = new Color(0, 0, .75f, 0f);
+        Color purple = new Color(1f, 0f, .68f, 0f);
+        Color lile = new Color(.21f, .20f, .67f, 0f);
         foreach(Entity pool in PoolsManager.gamePools.Where(pool => pool.Pool.Effects.Any(effect => effect.Name == "buff_singularity")))
         {
             poolId = (int)pool.Id;
             poolVFX = PoolsManager.poolsVFXs[$"{poolId}_1"].GetComponentInChildren<VisualEffect>();
-            poolVFX.SetVector4("Color A", red);
-            poolVFX.SetVector4("Color B", blue);
+            poolVFX.SetVector4("Color A", purple);
+            poolVFX.SetVector4("Color B", lile);
         }
     }
 
